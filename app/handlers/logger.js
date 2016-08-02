8367c84cf3bfdbf760a19fb4c855e28492f67e8c
s0rc3r3r01 <sorcerer10@gmail.com>
2016-08-02 23:35:53 +0100

    updated logging function

--- app/handlers/logger.js
+++ app/handlers/logger.js
@@ -12,20 +12,13 @@ var logstash = new Logstash({
 
 
 exports.logEvent = function(obj, callback) {
-obj.timestamp = new Date();
-obj.containerid = process.env.hostname;
-obj.machineid = process.env.host_hostname;
-logstash.send(obj);
-if (obj.level == 'error') {
-    console.log(obj.message   .red);
-} else {
-    console.log(obj.message);
-}
-if (err) {
-    console.error("REDIS ERROR".red);
-    callback(err);
-}
-console.log("views for user incremented " + user.yellow);
-callback(null, reply);
-});
+    obj.timestamp = new Date();
+    obj.containerid = process.env.hostname;
+    obj.machineid = process.env.host_hostname;
+    logstash.send(obj);
+    if (obj.level == 'error') {
+        console.log(obj.message.red);
+    } else {
+        console.log(obj.message);
+    }
 }
